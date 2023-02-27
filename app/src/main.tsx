274e35918789af6afe2e3466cd6e07e4a272e951
Dawid Badurek <56804600+TheDoom-IT@users.noreply.github.com>
2023-02-27 21:40:07 +0100

    Add CI/CD and Github pages
    * Add first workflow
    
    * Add CI for the package and app
    
    * Update descriptions of github actions

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -12,36 +12,41 @@ import {
 import { AppTestRoute } from './routes/app-test-route';
 import './styles/index.css';
 
-export const browserRouter = createBrowserRouter([
+export const browserRouter = createBrowserRouter(
+    [
+        {
+            path: ROUTES.HOME,
+            element: <HomeRoute />,
+        },
+        {
+            path: ROUTES.STATIC,
+            element: <StaticRoute />,
+        },
+        {
+            path: ROUTES.DIV_CHANGE,
+            element: <DivChangeRoute />,
+        },
+        {
+            path: ROUTES.RECREATE_CANVAS,
+            element: <RecreateCanvasRoute />,
+        },
+        {
+            path: ROUTES.CHILDREN_TEST,
+            element: <ChildrenTestRoute />,
+        },
+        {
+            path: ROUTES.APP_TEST,
+            element: <AppTestRoute />,
+        },
+        {
+            path: ROUTES.APP,
+            element: <AppRoute />,
+        },
+    ],
     {
-        path: ROUTES.HOME,
-        element: <HomeRoute />,
+        basename: import.meta.env.BASE_URL,
     },
-    {
-        path: ROUTES.STATIC,
-        element: <StaticRoute />,
-    },
-    {
-        path: ROUTES.DIV_CHANGE,
-        element: <DivChangeRoute />,
-    },
-    {
-        path: ROUTES.RECREATE_CANVAS,
-        element: <RecreateCanvasRoute />,
-    },
-    {
-        path: ROUTES.CHILDREN_TEST,
-        element: <ChildrenTestRoute />,
-    },
-    {
-        path: ROUTES.APP_TEST,
-        element: <AppTestRoute />,
-    },
-    {
-        path: ROUTES.APP,
-        element: <AppRoute />,
-    },
-]);
+);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <RouterProvider router={browserRouter} />,
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    resolve: {
-        alias: {
-            three: path.resolve('./node_modules/three'),
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+        base: env.BASE_URL,
+        resolve: {
+            alias: {
+                three: path.resolve('./node_modules/three'),
+            },
         },
-    },
-    plugins: [react()],
+        plugins: [react()],
+    };
 });
