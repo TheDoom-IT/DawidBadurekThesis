f1d49b373c250886afbe3626821013284440cebb
Dawid Badurek <dawidbadurek@wp.pl>
2023-04-05 16:36:49 +0200

    Update consecutive line segments to fade away

--- app/src/components/track-fragment.tsx
+++ app/src/components/track-fragment.tsx
@@ -1,8 +1,8 @@
-import { useMemo } from 'react';
+import { useCallback } from 'react';
 import {
     BufferGeometry,
-    Line,
     LineBasicMaterial,
+    LineSegments,
     Points,
     PointsMaterial,
 } from 'react-three-component';
@@ -44,45 +44,61 @@ export const TrackFragment = ({ track }: TrackFragmentProps) => {
             return;
         }
 
-        const vertices = new Array(LINE_SEGMENTS).fill(new THREE.Vector3(0, 0, 0));
-        buffer.setFromPoints(vertices);
+        const vertices: number[] = new Array(LINE_SEGMENTS * 2 * 3).fill(0);
+
+        const colors: number[] = [];
+        const count = LINE_SEGMENTS > track.count ? track.count : LINE_SEGMENTS;
+        for (let x = 0; x < count; x++) {
+            const color1 = 1 - x / count;
+            const color2 = 1 - (x + 1) / count;
+            colors.push(color1, color1, color1, color1, color2, color2, color2, color2);
+        }
+
+        buffer.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+        buffer.setAttribute('color', new THREE.Float32BufferAttribute(colors, 4));
     };
 
-    const lineAnimation = useMemo(() => {
-        return (ref: THREE.BufferGeometry, timestamp: number) => {
-            const position = ref.getAttribute('position');
-
-            const index = Math.floor(timestamp / ANIMATION_SPEED) % track.count;
-            for (let x = 0; x < LINE_SEGMENTS; ++x) {
-                updatePosition(x, index < x ? 0 : index - x, position, track);
-            }
-            position.needsUpdate = true;
-        };
-    }, [track]);
-
-    const pointsAnimation = useMemo(() => {
-        return (ref: THREE.BufferGeometry, timestamp: number) => {
+    const pointsAnimation = useCallback(
+        (ref: THREE.BufferGeometry, timestamp: number) => {
             const position = ref.getAttribute('position');
 
             const index = Math.floor(timestamp / ANIMATION_SPEED) % track.count;
             updatePosition(0, index, position, track);
             position.needsUpdate = true;
-        };
-    }, [track]);
+        },
+        [track],
+    );
 
-    const color = useMemo(() => {
-        return 0xffffff * Math.random();
-    }, []);
+    const lineAnimation = useCallback(
+        (ref: THREE.BufferGeometry, timestamp: number) => {
+            const position = ref.getAttribute('position');
+
+            const index = Math.floor(timestamp / ANIMATION_SPEED) % track.count;
+            for (let x = 0; x < LINE_SEGMENTS; ++x) {
+                updatePosition(x * 2, index < x ? 0 : index - x, position, track);
+                updatePosition(x * 2 + 1, index < x + 1 ? 0 : index - (x + 1), position, track);
+            }
+            position.needsUpdate = true;
+        },
+        [track],
+    );
 
     return (
         <>
-            <Line>
+            <LineSegments>
                 <BufferGeometry animate={lineAnimation} innerRef={setLine} />
-                <LineBasicMaterial params={[{ color }]} />
-            </Line>
+                <LineBasicMaterial
+                    params={[
+                        {
+                            vertexColors: true,
+                            transparent: true,
+                        },
+                    ]}
+                />
+            </LineSegments>
             <Points>
                 <BufferGeometry animate={pointsAnimation} innerRef={setPoints} />
-                <PointsMaterial params={[{ color }]} />
+                <PointsMaterial params={[{ color: 0xffffff }]} />
             </Points>
         </>
     );
