27321d0e695e6cc0ff89694d526373b176811175
Dawid Badurek <dawidbadurek@wp.pl>
2023-05-01 10:14:36 +0200

    Fix effect types

--- package/src/components/postprocessing/effects.ts
+++ package/src/components/postprocessing/effects.ts
@@ -3,10 +3,14 @@ import * as POST from 'postprocessing';
 import { createEffect } from '../../utils/create-effect';
 
 export type BloomEffectProps = ParamsProps<typeof POST.BloomEffect, POST.BloomEffect>;
-export const BloomEffect = createEffect(POST.BloomEffect);
+export const BloomEffect = createEffect<typeof POST.BloomEffect, POST.BloomEffect>(
+    POST.BloomEffect,
+);
 
 export type FXAAEffectProps = ParamsProps<typeof POST.FXAAEffect, POST.FXAAEffect>;
-export const FXAAEffect = createEffect(POST.FXAAEffect);
+export const FXAAEffect = createEffect<typeof POST.FXAAEffect, POST.FXAAEffect>(POST.FXAAEffect);
 
 export type NoiseEffectProps = ParamsProps<typeof POST.NoiseEffect, POST.NoiseEffect>;
-export const NoiseEffect = createEffect(POST.NoiseEffect);
+export const NoiseEffect = createEffect<typeof POST.NoiseEffect, POST.NoiseEffect>(
+    POST.NoiseEffect,
+);
