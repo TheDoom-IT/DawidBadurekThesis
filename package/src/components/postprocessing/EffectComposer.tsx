b7d2411d17ad009b2531804e83e94d912141af78
Dawid Badurek <dawidbadurek@wp.pl>
2023-05-10 15:27:38 +0200

    Add small improvements

--- package/src/components/postprocessing/EffectComposer.tsx
+++ package/src/components/postprocessing/EffectComposer.tsx
@@ -4,43 +4,46 @@ import React, { ForwardedRef, useLayoutEffect } from 'react';
 import { useCanvasContext } from '../../contexts/canvas-context';
 import { useDisposableObject } from '../../hooks/useDisposableObject';
 
-export type EffectComposerProps = ParamsProps<typeof POST.EffectComposer, POST.EffectComposer>;
+export const EffectComposer = React.forwardRef(function EffectComposer(
+    props: ParamsProps<typeof POST.EffectComposer, POST.EffectComposer>,
+    ref: ForwardedRef<POST.EffectComposer>,
+) {
+    const canvasContext = useCanvasContext();
+    const composer = useDisposableObject(POST.EffectComposer, props, ref);
 
-export const EffectComposer = React.forwardRef<POST.EffectComposer, EffectComposerProps>(
-    function EffectComposer(props: EffectComposerProps, ref: ForwardedRef<POST.EffectComposer>) {
-        const canvasContext = useCanvasContext();
-        const composer = useDisposableObject(POST.EffectComposer, props, ref);
+    const contextAttributes = canvasContext?.renderer?.getContext()?.getContextAttributes();
+    const composerRenderer = composer?.getRenderer();
 
-        const contextAttributes = canvasContext?.renderer?.getContext()?.getContextAttributes();
-        const composerRenderer = composer?.getRenderer();
+    useLayoutEffect(() => {
+        if (!canvasContext?.renderer || !composer || !contextAttributes) {
+            return;
+        }
 
-        useLayoutEffect(() => {
-            if (!canvasContext?.renderer || !composer || !contextAttributes) {
-                return;
-            }
+        composer.setRenderer(canvasContext.renderer);
+    }, [composer, canvasContext?.renderer, contextAttributes]);
 
-            composer.setRenderer(canvasContext.renderer);
-        }, [composer, canvasContext?.renderer, contextAttributes]);
+    useLayoutEffect(() => {
+        if (!composer || !canvasContext?.size || !composerRenderer) {
+            return;
+        }
 
-        useLayoutEffect(() => {
-            if (!composer || !canvasContext?.size || !composerRenderer) {
-                return;
-            }
+        composer.setSize(canvasContext.size.width, canvasContext.size.height, false);
+    }, [
+        composer,
+        canvasContext?.size,
+        canvasContext?.size?.width,
+        canvasContext?.size?.height,
+        composerRenderer,
+    ]);
 
-            composer.setSize(canvasContext.size.width, canvasContext.size.height, false);
-        }, [
-            composer,
-            canvasContext?.size,
-            canvasContext?.size?.width,
-            canvasContext?.size?.height,
-            composerRenderer,
-        ]);
+    useLayoutEffect(() => {
+        canvasContext?.setEffectComposer(composer);
 
-        useLayoutEffect(() => {
-            canvasContext?.setEffectComposer(composer);
-            // eslint-disable-next-line react-hooks/exhaustive-deps
-        }, [composer, canvasContext?.setEffectComposer]);
+        return () => {
+            canvasContext?.setEffectComposer(null);
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [composer, canvasContext?.setEffectComposer]);
 
-        return <>{props.children}</>;
-    },
-);
+    return <>{props.children}</>;
+});
