f205998762bc1b961b21fdfe7781a07c861bd253
Dawid Badurek <dawidbadurek@wp.pl>
2023-05-13 10:41:24 +0200

    Add testing files

--- app/tst.ts
+++ app/tst.ts
@@ -1,33 +1,34 @@
 import * as fs from 'fs';
 
-const file = fs.readFileSync('./track-100.json');
+const file = fs.readFileSync('./src/static/exemplary-data.json');
 const data = JSON.parse(file.toString());
 
-function getRandom(size: number): number[] {
+const TRACKS_NUMBER = 10;
+const POINTS_NUMBER = 100;
+
+function getRandom(size: number, range = 100): number[] {
     const result = new Array(size);
     for (let x = 0; x < result.length; x++) {
-        result[x] = Math.random() * 100 * (Math.random() > 0.5 ? 1 : -1);
+        result[x] = Math.random() * range * (Math.random() > 0.5 ? 1 : -1);
     }
     return result;
 }
 
-// data['mCalo'] = [];
-// data['mTracks'] = data['mTracks'].slice(0, 100);
-// //@ts-ignore
-// data['mTracks'].forEach((track, index) => {
-//     data['mTracks'][index] = {
-//         ...track,
-//         count: 100,
-//         mPolyX: getRandom(100),
-//         mPolyY: getRandom(100),
-//         mPolyZ: getRandom(100),
-//     };
-// });
+data['mCalo'] = [];
+data['mTracks'] = data['mTracks'].slice(0, 1);
 
+const track = data['mTracks'][0];
 const toAdd = [];
-for (let x = 0; x < 9; x++) {
-    toAdd.push(...JSON.parse(JSON.stringify(data['mTracks'])));
+for (let x = 0; x < TRACKS_NUMBER; x++) {
+    toAdd.push({
+        ...track,
+        time: getRandom(1, 1000)[0],
+        count: POINTS_NUMBER,
+        mPolyX: getRandom(POINTS_NUMBER),
+        mPolyY: getRandom(POINTS_NUMBER),
+        mPolyZ: getRandom(POINTS_NUMBER),
+    });
 }
-data['mTracks'].push(...toAdd);
+data['mTracks'] = toAdd;
 
 fs.writeFileSync('output.json', JSON.stringify(data));
